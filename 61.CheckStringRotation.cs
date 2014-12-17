d6f9a7a03688a99db9ab3f073e6db31c38a7984e
xieqilu <xieqilu1989@gmail.com>
2014-12-17 10:52:56 -0800

    Create 61.CheckStringRotation.cs

---
+++ 61.CheckStringRotation.cs
@@ -0,0 +1,41 @@
+//Determine if two strings are right rotation of each other
+//OR determine if string1 is a right rotation of string2
+
+//Note: if string1 is a right rotation of string2,
+//then string2 must also be a right rotaion of string1
+
+//firstly, if the length of two strings are different,
+//Then theay cannot be right rotation of each other
+
+//We can concatenate string1 (double string1) and then
+//check if the new string1 contains string2.
+
+using System;
+
+namespace StringRotation
+{
+	public class Finder
+	{
+		public static bool CheckRotation(string str1, string str2)
+		{
+			if (str1.Length != str2.Length)
+				return false;
+			string newStr = string.Concat (str1, str1); //string.concat concatenate two strings
+			if (newStr.Contains (str2))
+				return true;
+			else
+				return false;
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string str1 = "abcdefg";
+			string str2 = "efgabcd";
+			string str3 = "gabcdef";
+			Console.WriteLine (Finder.CheckRotation (str2,str3));
+		}
+	}
+}
