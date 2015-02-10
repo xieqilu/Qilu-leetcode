bdf213a946f5d0536bdd71301b08963eb932d770
xieqilu <xieqilu1989@gmail.com>
2015-02-09 23:33:49 -0800

    Update 28.MaxSubArraySum.cs

--- 28.MaxSubArraySum.cs
+++ 28.MaxSubArraySum.cs
@@ -1,6 +1,14 @@
 //Find the Contiguous subarray within an array(containing at least one number) 
 //which has the largest sum.
 
+/**
+ * Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+the contiguous subarray [4,−1,2,1] has the largest sum = 6.
+
+Edge case: all elements in the array are negative value;
+*/
+
 using System;
 using System.Collections;
 
