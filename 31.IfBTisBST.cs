d42bfb106129042f78e6d2874cc874e98fb56802
xieqilu <xieqilu1989@gmail.com>
2015-02-25 23:13:00 -0800

    Update 31.IfBTisBST.cs

--- 31.IfBTisBST.cs
+++ 31.IfBTisBST.cs
@@ -4,63 +4,58 @@ using System;
 namespace DeterminBST
 {
 
-	class TreeNode
+	public class TreeNode
 	{
-		public TreeNode Left{ get; set;}
-		public TreeNode Right{ get; set;}
-		public int Value{ get; private set;}
-		public TreeNode(int value)
+		public TreeNode left{ get; set;}
+		public TreeNode right{ get; set;}
+		public int val{ get; private set;}
+		public TreeNode(int val)
 		{
-			this.Value = value;
-			this.Left = null;
-			this.Right = null;
+			this.val = val;
+			this.left = null;
+			this.right = null;
 		}
 	}
 
-	class Checker
-	{
-		private bool IsBSTHelper(TreeNode root, int high, int low)//Time: O(n) Space: O(1)
-																  //visit each node only once	
-		{
-			if (root == null)
-				return true; //must return true
-			if (root.Value < high && root.Value > low)
-				return (IsBSTHelper (root.Left, root.Value, low) && IsBSTHelper (root.Right, high, root.Value));
-			else
-				return false;
+	public class Solution1 {
+		//private object prev;
+		public static bool IsValidBST(TreeNode root) {
+			if(root==null)
+				return true;
+			//Note: do not use Int32.MinValue, because the value of node could be Int32.MinValue also!!!
+			//Instead, use an object to box the int value. Then we can use null to distinguish the initial value.
+			object prev=null; 
+			return IsValidBSTHelper(root,ref prev);
 		}
-
-		public bool IsBST(TreeNode root)
-		{
-			if (root == null)
-				return false; //edge case: root == null
-			int high = Int32.MaxValue;
-			int low = Int32.MinValue;
-			return IsBSTHelper (root, high, low);
-		}
-
-		private bool InOrderHelper(TreeNode root,ref int prev)//Time: O(n) Space:O(1)
-		{												  // In order Travesal of a binary search tree
-			if (root == null)							  // will output the node value strictly in increasing order!
-				return true;							  
-			if (InOrderHelper (root.Left,ref prev)) {
-				if (root.Value > prev) {
-					prev = root.Value;
-					return InOrderHelper (root.Right, ref prev);
-				} else
-					return false;
-			} else
+		private static bool IsValidBSTHelper(TreeNode root,ref object prev){ //Note: must pass by reference to make prev consistant
+			if(root == null) return true; //null is a valid BST
+			if(IsValidBSTHelper(root.left,ref prev)){
+				if(prev==null || root.val>(int)prev){ //unbox prev to get its int value
+					prev = root.val;
+					return IsValidBSTHelper(root.right,ref prev);
+				}
 				return false;
-		}
-
-		public bool IsBSTInOrder(TreeNode root)
-		{
-			if (root == null)
-				return false;
-			int prev = Int32.MinValue;
-			return InOrderHelper (root, ref prev);
+			}
+			return false;
 		}
 	}
+
+	public class Solution2 {
+		public static bool IsValidBST(TreeNode root) {
+			//Do not use Int32.MaxValue and Int32.MinValue, becasue the value of tree node may be these two values too.
+			//Instead use object to box int value and pass two null objects as initial value.
+			return IsValidBSTHelper(root, null, null); 
+		}
+
+		private static bool IsValidBSTHelper(TreeNode p, object low, object high) {
+			if (p == null) return true;
+			return (low == null || p.val > (int)low) && (high == null || p.val < (int)high) //unbox object when comparing value
+				&& IsValidBSTHelper(p.left, low, (object)p.val) //box int and pass the object
+				&& IsValidBSTHelper(p.right, (object)p.val, high); //box int and pass the object
+		}
+	}
+
+
 	class MainClass
 	{
 		public static void Main (string[] args)
@@ -74,16 +69,16 @@ namespace DeterminBST
 			TreeNode nodeE = new TreeNode (4);
 			TreeNode nodeF = new TreeNode (2);
 
-			root.Left = nodeA;
-			root.Right = nodeC;
-			nodeA.Left = nodeF;
-			nodeA.Right = nodeE;
-			nodeC.Left = nodeB;
-			nodeC.Right = nodeD;
+			root.left = nodeA;
+			root.right = nodeC;
+			nodeA.left = nodeF;
+			nodeA.right = nodeE;
+			nodeC.left = nodeB;
+			nodeC.right = nodeD;
 
-			Checker checker = new Checker ();
-			Console.WriteLine(checker.IsBST (root));
-			Console.WriteLine (checker.IsBSTInOrder (root));
+			//The two solutions are already tested by all Leetcode test cases!!!
+			Console.WriteLine(Solution1.IsValidBST(root));//true
+			Console.WriteLine (Solution2.IsValidBST (root)); //true
 		}
 	}
 }
