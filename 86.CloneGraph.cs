0524952863aa2d64d592bc73b318ca547556f4c2
xieqilu <xieqilu1989@gmail.com>
2015-02-28 18:36:23 -0800

    Update 86.CloneGraph.cs

--- 86.CloneGraph.cs
+++ 86.CloneGraph.cs
@@ -1,14 +1,15 @@
 /**
  * Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
  * 
+ * 
  * Definition for undirected graph.
- * class UndirectedGraphNode {
- *     int label;
- *     List<UndirectedGraphNode> neighbors;
- *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * public class UndirectedGraphNode {
+ *     public int label;
+ *     public IList<UndirectedGraphNode> neighbors;
+ *     public UndirectedGraphNode(int x) { label = x; neighbors = new List<UndirectedGraphNode>(); }
  * };
  * 
- * Solution:
+ * Solution 1: BFS
  * use a queue to do BFS on the graph
  * use a HashMap<Node, Node> to store each Node and its copy
  * Also use the HashMap to check if a Node is already been visited
@@ -27,6 +28,18 @@
  * node. Then add the neighbor itself to the queue.
  * 
  * After the loop, return the copy of the head node.
+ * 
+ * Solution 2: DFS
+ * Use a Recursive DFS approach
+ * A HashMap will be used through the entire recursive stack
+ * The key of map is the original node and value is the copied node
+ * In the recursive call, if the input node is already in the map,
+ * we directly return the copy of the input node from the map.
+ * If it is not in the map yet. we creat a copy and put the node
+ * and its copy to the map. 
+ * Then for each of its neighbors, we use the recursive call to get
+ * the copy and put the copy into the curent copyNode's list.
+ * 
  */
 
 using System;
@@ -36,25 +49,25 @@ namespace CloneGraph
 {
 	public class UndirectedGraphNode {
 		public int label{ get; set;}
-		public List<UndirectedGraphNode> neighbors{ get; set;}
+		public IList<UndirectedGraphNode> neighbors{ get; set;}
 		public UndirectedGraphNode(int x) { 
 			label = x;
 			neighbors = new List<UndirectedGraphNode>(); 
 		}
 	}
 
-	public class Finder{
-		public static UndirectedGraphNode cloneGraph(UndirectedGraphNode node){
-			if (node == null)
+	//BFS Iterative Solution
+	public class Solution {
+    		public UndirectedGraphNode CloneGraph(UndirectedGraphNode node) {
+        		if (node == null)
 				return null;
-			Dictionary<UndirectedGraphNode, UndirectedGraphNode> dic = new Dictionary<UndirectedGraphNode, UndirectedGraphNode> ();
-			Queue<UndirectedGraphNode> q = new Queue<UndirectedGraphNode> ();
+			Dictionary<UndirectedGraphNode, UndirectedGraphNode> dic = new Dictionary<UndirectedGraphNode, UndirectedGraphNode> ();			Queue<UndirectedGraphNode> q = new Queue<UndirectedGraphNode> ();
 			UndirectedGraphNode newNode = new UndirectedGraphNode (node.label);
 			q.Enqueue (node);
 			dic.Add (node, newNode);
 			while (q.Count != 0) {
 				UndirectedGraphNode currNode = q.Dequeue ();
-				List<UndirectedGraphNode> neighbors = currNode.neighbors;
+				IList<UndirectedGraphNode> neighbors = currNode.neighbors;
 				foreach (UndirectedGraphNode neighbor in neighbors) {
 					if (!dic.ContainsKey (neighbor)) {
 						q.Enqueue (neighbor);
@@ -66,8 +79,28 @@ namespace CloneGraph
 				}
 			}
 			return newNode;
-		}
-	}
+    }
+    
+ //DFS Recursive Solution
+ public class Solution {
+    public UndirectedGraphNode CloneGraph(UndirectedGraphNode node) {
+        if(node==null) return null;
+        Dictionary<UndirectedGraphNode,UndirectedGraphNode> dic = new Dictionary<UndirectedGraphNode,UndirectedGraphNode>();
+        return DFS(node,dic);
+    }
+    
+    private UndirectedGraphNode DFS(UndirectedGraphNode node, Dictionary<UndirectedGraphNode,UndirectedGraphNode> dic){
+        if(dic.ContainsKey(node))
+            return dic[node];
+        UndirectedGraphNode copyNode = new UndirectedGraphNode(node.label);
+        dic.Add(node,copyNode);
+        foreach(UndirectedGraphNode neighbor in node.neighbors){
+            copyNode.neighbors.Add(DFS(neighbor,dic));
+        }
+        return copyNode;
+    }
+}
+}
 
 	class MainClass
 	{
