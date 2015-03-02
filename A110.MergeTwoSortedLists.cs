9a09ef49c429f7124c6b4a755b2dcd55f96c48d9
xieqilu <xieqilu1989@gmail.com>
2015-03-01 16:36:54 -0800

    Update A110.MergeTwoSortedLists.cs

--- A110.MergeTwoSortedLists.cs
+++ A110.MergeTwoSortedLists.cs
@@ -35,7 +35,7 @@
  
 //This is my solution, which is the same as Leetcode official Solution!!!
 public class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
         if(l1==null||l2==null){
             if(l1 ==null) return l2;
             if(l2 ==null) return l1;
