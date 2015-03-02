bdeec320101a357b82c56c4f39bbe0d365dba63b
xieqilu <xieqilu1989@gmail.com>
2015-03-02 15:45:04 -0800

    Update and rename 41.WordBreakDP.cs to 41.WordBreak.cs

--- 41.WordBreak.cs
+++ 41.WordBreak.cs
@@ -1,11 +1,42 @@
-//Given a string s and a dictionary of words dict, 
-//determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+/**
+ * Given a string s and a dictionary of words dict, 
+ * determine if s can be segmented into a space-separated sequence of one or more dictionary words.
 
-//For example, given
-//s = "leetcode",
-//dict = ["leet", "code"].
+For example, given
+s = "leetcode",
+dict = ["leet", "code"].
 
-//Return true because "leetcode" can be segmented as "leet code".
+Return true because "leetcode" can be segmented as "leet code".
+*/
+
+/**
+ * Solution:
+ * Do not use Recursive solution to solve the problem, it's very inefficient.
+ * Use Iterative Dynamic Programming approach as follows:
+ * 
+ * Use a bool array dp to keep track of which part of the input string is already matched.
+ * The size of bool array is the size of Set+1. And dp[i] is true means all the chars of 
+ * s (input string) before s[i] is already matched. So initiliaze dp[0] as true, then we
+ * can start matching from s[0].
+ * 
+ * Then iterate through each char of s, for each char s[i], check if dp[i] is true. If dp[i]
+ * is false, no need to do match from current position. If dp[i] is true, which means all the
+ * chars before s[i] are already matched. So we try to match chars from s[i].
+ * 
+ * To match chars from s[i], we iterate through dict. For each string str, try to match
+ * s.substring(i,i+str.length()) with str. If matched, set dp[i+str.length()] to true and 
+ * check if i+str.length() is equal to s.length(), if it is, then all chars of s are matched,
+ * we break the loop. If not matched, continue.
+ * 
+ * After the above nested loop, return dp[s.length()], which indicates if all chars of s are matched
+ * or not.
+ * 
+ * 
+ * Time Complexity: O(n*m), n is the size of input string, m is the size of input dict
+ * Space Complexity: O(n), n is the size of input string.
+ * 
+ * */
+
 
 //This file contains two solutions: Naive/Recursive Approach And DP Approach
 
@@ -58,15 +89,13 @@ namespace WordBreak
 							if (str.Substring (i, len).Equals (s)) {
 								temp [end] = true;
 								if (end == str.Length)
-									break;
+									return true;
 								}
 							}
 					}
-					if (temp [str.Length] == true)
-						break;
 				}
 			}
-			return temp [str.Length]; //last element of temp indicate if the whole str can be matched
+			return false; //last element of temp indicate if the whole str can be matched
 		}
 	}
 
