beef88912a8d92c29a02e09ccc251bd231f88e37
xieqilu <xieqilu1989@gmail.com>
2015-03-18 14:26:13 -0700

    Update 85.TwoSum.cs

--- 85.TwoSum.cs
+++ 85.TwoSum.cs
@@ -22,6 +22,13 @@ Output: index1=1, index2=2
  * 
  * Note we must use element as key and index as value, because
  * we need to find if hashMap contains key fast.
+ *
+ * Special Note: 
+ * In the loop, first check if numbers[i] already exists in dict, if not, add numbers[i] to dict. 
+ * Otherwise, would cause an error.
+ * If there are multiple element A in array, and A is a part of two results. 
+ * We need to know which index of A to consider as result. If it's the last A, then 
+ * we need to update dict[A] if A already exists in array. If it's the first A, don't update.
  * */
 
 using System;
@@ -31,15 +38,21 @@ namespace TwoSum
 {
 	class Finder{
 		public static int[] TwoSum(int[] numbers, int target){
-			Dictionary<int,int> dic = new Dictionary<int, int> ();
+			Dictionary<int,int> dict = new Dictionary<int, int> ();
 			int[] result = new int[2];
 			for (int i = 0; i < numbers.Length; i++) {
-				if (dic.ContainsKey (target - numbers [i])) {
+				if (dict.ContainsKey (target - numbers [i])) {
 					result [0] = dic [target - numbers [i]] + 1;
 					result [1] = i + 1;
 					break;
-				} else
-					dic.Add (numbers [i], i);
+				} 
+				//if numbers[i] appears before, don't add to dict, will cause an error
+            			else if(!dict.ContainsKey(numbers[i])) 
+                			dict.Add(numbers[i],i);
+                		//if there are multiple a in array, and we need to return the index of last a,use below code
+                		//if we need to return the index of first a, don't use below code
+                		//else
+                		//	dict[numbers[i]]=i;
 			}
 			return result;
 		}
