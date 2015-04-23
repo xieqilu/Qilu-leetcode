7bb01f4b465df0c33aeb480509f9fca008c0e994
xieqilu <xieqilu1989@gmail.com>
2015-04-23 15:42:30 -0700

    Create PredictOutput1.cpp

---
+++ CPP-LeetCode/PredictOutput1.cpp
@@ -0,0 +1,47 @@
+//What does the following program print?
+
+#include <iostream>
+
+using namespace std;
+
+class A {
+  public:
+	A() {cout<<"Constructor A\n";}
+    virtual ~A() {cout << "Destructor A\n";}
+    virtual const char* Classname() const{return "A";}
+};
+
+
+class B : public A {
+public:
+    B() {cout<<"Constructor B\n";}
+    virtual ~B() {cout << "Destructor B\n";}
+    virtual const char* Classname() const{return "B";}
+};
+
+
+void foo(A *a) {
+    printf("foo has been passed an object of class %s\n", a->Classname());
+    delete a;
+}
+
+int main(int, char**) {
+    A *a = new A;
+    B *b = new B;
+    foo(a);
+    foo(b);
+    
+    return (0);
+}
+
+/**
+Output:
+Constructor A
+Constructor A
+Constructor B
+foo has been passed an object of class A
+Destructor A
+foo has been passed an object of class B
+Destructor B
+Destructor A
+*/
