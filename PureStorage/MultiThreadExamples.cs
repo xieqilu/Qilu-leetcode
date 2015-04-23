bd513f960938d79756f229989861e3986fc32418
xieqilu <xieqilu1989@gmail.com>
2015-04-23 09:01:14 -0700

    Update MultiThreadExamples.cs

--- PureStorage/MultiThreadExamples.cs
+++ PureStorage/MultiThreadExamples.cs
@@ -48,6 +48,68 @@ namespace TestConsoleApp
 }
 
 /**
+Note: From .Net FrameWork 2.0, We can start and use Thread more easily.
+1, We don't need to create a delegate (ThreadStart) explicitly. We can directly spicy the method name into
+the Thread constructor and the compiler will selects the correct delegate.
+*/
+
+//Example for using Thread more easily
+using System;
+using System.Threading;
+
+class Test
+{
+    static void Main() 
+    {
+        // To start a thread using a static thread procedure, use the 
+        // class name and method name when you create the ThreadStart 
+        // delegate. Beginning in version 2.0 of the .NET Framework, 
+        // it is not necessary to create a delegate explicitly.  
+        // Specify the name of the method in the Thread constructor,  
+        // and the compiler selects the correct delegate. For example: 
+        // 
+         Thread newThread = new Thread(Work.DoWork); 
+        //
+        // ThreadStart threadDelegate = new ThreadStart(Work.DoWork);
+        // Thread newThread = new Thread(threadDelegate);
+        newThread.Start();
+
+        // To start a thread using an instance method for the thread  
+        // procedure, use the instance variable and method name when  
+        // you create the ThreadStart delegate. Beginning in version 
+        // 2.0 of the .NET Framework, the explicit delegate is not 
+        // required. 
+        //
+        Work w = new Work();
+        w.Data = 42;
+        newThread = new Thread(w.DoMoreWork); //directly attach newThread to a new method
+        // threadDelegate = new ThreadStart(w.DoMoreWork);
+        // newThread = new Thread(threadDelegate);
+        newThread.Start();
+    }
+}
+
+class Work 
+{
+    public static void DoWork() 
+    {
+        Console.WriteLine("Static thread procedure."); 
+    }
+    public int Data;
+    public void DoMoreWork() 
+    {
+        Console.WriteLine("Instance thread procedure. Data={0}", Data); 
+    }
+}
+
+/* This code example produces the following output (the order 
+   of the lines might vary):
+Static thread procedure.
+Instance thread procedure. Data=42
+ */
+
+
+/**
 Lets move one step ahead to see how more than one threads can be run in parallel. 
 Following code snippet will demonstrate such scenario. Though, I am using two threads only here, 
 you can add as many you want to it as per your need.
